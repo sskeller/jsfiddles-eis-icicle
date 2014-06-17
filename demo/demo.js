3b37503fd62cac3d89b2b972bdd8516f2264830b
Scott Keller <scott.keller13@gmail.com>
2014-06-17 16:33:41 -0400

    Start building icicle chart

--- demo/demo.js
+++ demo/demo.js
@@ -7,6 +7,23 @@ var EIS = window.EIS || {};
   _.extend(EIS, {
   });
 
+  var width = 945;
+  var height = 300;
+  var x = d3.scale.linear().range([0, width]);
+  var y = d3.scale.linear().range([0, height]);
+  var color = d3.scale.category20c();
+  var partition = d3.layout.partition();
+
+  var svg = d3.select("#icicle").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  var rect = svg.selectAll("rect");
+  var isJsFiddle = /jsfiddle[.]net$/.test(location.host);
+  var jsonURL = isJsFiddle ? "http://sskeller.github.io/jsfiddles-eis-icicle/json/icicle.json" : "json/icicle.json";
+
+  alert(jsonURL);
+
   $(function() {
 
     /*---------- Shim to treat CSS panel as Less ----------*/
